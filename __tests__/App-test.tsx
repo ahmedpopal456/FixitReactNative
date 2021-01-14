8dc0e4b95dd69ff278c6c2078de5c8551e2bb35c
Jack Leung <jack-_-leung@hotmail.com>
2021-01-14 02:31:43 +0000

    Merged PR 78: Integration of B2C to React Native
    Features that are added:
    - Sign Up
    - Log In
    - Get Token (once you are logged in, there is a Get Token button that returns the token. But the moment you log in, the token is already returned.)
    - Sign Out
    
    https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview#linking-user-flows
    To handle the Forgot Password, the application will receive the error code as mentioned in the documentation.
    
    How it looks when you run the app:
    ![image (2).png](https://dev.azure.com/FixIt-App/08d378fd-be72-4c80-9ce4-1a7c754efb98/_apis/git/repositories/18d03e95-5f45-4fea-8845-fc0cd92f2ab1/pullRequests/78/attachments/image%20%282%29.png)
    
    Related work items: #278

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,8 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('../b2c/b2cClient.ts');
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,16 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
-import { Button, Icon } from 'fixit-mobile-components';
+import RegisterPage from '../components/registerPage';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Button onPress={() => console.log("Button Pressed")} color="accent" shape="circle">
-          <Icon library="AntDesign" name="barschart" color="primary" />
-        </Button>
+        <RegisterPage />
       </SafeAreaView>
     </>
   );
