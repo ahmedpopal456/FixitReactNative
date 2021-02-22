e5ad58b9e3a2155cd1a62e5e69b542a46ce3142c
Paul Musat <pmusat@outlook.com>
2021-02-21 23:10:34 -0500

    #441 Configured the application for Android notifications

--- android/app/src/main/java/com/fixitreactnative/MainActivity.java
+++ android/app/src/main/java/com/fixitreactnative/MainActivity.java
@@ -2,6 +2,11 @@ package com.fixitreactnative;
 
 import com.facebook.react.ReactActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -12,4 +17,26 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "FixitReactNative";
   }
+
+  /**
+   * When the app is created, it will create a new notification channel for 
+   * android devices that support notification channels. This code is 
+   * needed in order for the notification heads-up
+   * banner to be displayed when the app is closed. Notifee could also be
+   * used to manage android notification channels but with caveats.
+   */
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {    
+              NotificationChannel notificationChannel = new NotificationChannel("fixit-default-channel", "Fixit", NotificationManager.IMPORTANCE_HIGH);
+              notificationChannel.setShowBadge(true);
+              notificationChannel.setDescription("Default notification channel used by Fixit");
+              notificationChannel.enableVibration(true);
+              notificationChannel.enableLights(true);
+              notificationChannel.setVibrationPattern(new long[]{400, 200, 400});
+              NotificationManager manager = getSystemService(NotificationManager.class);
+              manager.createNotificationChannel(notificationChannel);
+          }
+    }
 }
