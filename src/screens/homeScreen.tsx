1b2b9f1f6059a8e005c7ade5c103b17c4da5bed8
Mack <trenholm.mackenzie@gmail.com>
2021-03-04 20:43:25 -0500

    CSS shenanigans

--- src/screens/homeScreen.tsx
+++ src/screens/homeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Icon, Tag, Button } from 'fixit-common-ui';
 import SearchTextInput from '../components/SearchTextInput';
 
@@ -79,7 +79,35 @@ class HomeScreen extends
                 <>
                     <View style={{paddingTop:200, paddingLeft:10, paddingRight:10}}>
                         <View style={styles.content}>
-                            <View style={{flexDirection:'row'}}>                                    
+                            <View style={{flexDirection:'row'}}>
+
+                            <View style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            }}>
+                            {this.state.tags.map((tag:any) => (
+                                tag
+                                ? <View key={tag} style={{
+                                    flexGrow: 0,
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    marginRight: 10,
+                                    marginTop: 2,
+                                    marginLeft: 2
+                                }}>
+                                    <Tag backgroundColor={'accent'} textColor={'dark'}>{tag}</Tag>
+                                    <TouchableOpacity style={{ flexGrow: 0, marginLeft: -5 }}
+                                    onPress={() => this.removeTag(tag)}>
+                                    <Icon library="FontAwesome5" name="times-circle" color={'dark'} />
+                                    </TouchableOpacity>
+                                </View>
+                                : null
+                            ))}
+                        </View>
+
+                                <View style={{position:'absolute', zIndex: -1}}>
                                     <SearchTextInput
                                     onChange={(text : string) => this.setTagInputText(text)}
                                     value={this.state.tagInputText}
@@ -87,7 +115,8 @@ class HomeScreen extends
                                     onFocus={() => this.showTagSuggestions()}
                                     onSubmitEditing={this.addTag}
                                     />
-                                    <View style={{paddingLeft:40}}>
+                               </View>
+                                    <View style={{paddingLeft:250}}>
                                     <Button onPress={()=> console.log('button pressed')} color="primary" width={50} padding={0}>
                                     <Icon library="Ionicons" name="hammer-outline" color="accent"/>
                                     </Button>
@@ -125,29 +154,7 @@ class HomeScreen extends
                                         ))}
                             </View>
                         </View>
-                        <View style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            flexWrap: 'wrap',
-                            }}>
-                            {this.state.tags.map((tag:any) => (
-                                tag
-                                ? <View key={tag} style={{
-                                    flexGrow: 0,
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    marginRight: 10,
-                                }}>
-                                    <Tag backgroundColor={'accent'} textColor={'dark'}>{tag}</Tag>
-                                    <TouchableOpacity style={{ flexGrow: 0, marginLeft: -5 }}
-                                    onPress={() => this.removeTag(tag)}>
-                                    <Icon library="FontAwesome5" name="times-circle" color={'dark'} />
-                                    </TouchableOpacity>
-                                </View>
-                                : null
-                            ))}
-                        </View>
+                        
                     </View>
                 </>
               );
