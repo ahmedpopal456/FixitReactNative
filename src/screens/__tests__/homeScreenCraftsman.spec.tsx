e687660a8efcf57d073428ebaaf76099aededffd
Ahmed Popal <ahmed@empower.social>
2022-01-02 12:40:03 -0500

    Updated CommonDataStore Version & Regenerate Snapshots

--- src/screens/__tests__/homeScreenCraftsman.spec.tsx
+++ src/screens/__tests__/homeScreenCraftsman.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'react-native';
 import { shallow } from 'enzyme';
 import { Provider, store } from 'fixit-common-data-store';
-import HomeScreenCraftsman from '../homeScreenCraftsman';
+import HomeScreenCraftsman from '../home/homeScreenCraftsman';
 
 describe('Home Screen Craftsman', () => {
   it('renders correctly', () => {
