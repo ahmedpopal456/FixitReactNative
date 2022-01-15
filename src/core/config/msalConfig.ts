69254fa215a8bd7d10cccabbabeac96a512f960b
Ahmed Popal <ahmed@empower.social>
2022-01-15 17:41:42 -0500

    updated dev name of the application

--- src/core/config/msalConfig.ts
+++ src/core/config/msalConfig.ts
@@ -4,7 +4,7 @@ export const b2cConfig: B2CTypes.B2CConfiguration = {
   auth: {
     clientId: '60d0d7ce-85ff-427d-ae26-8e1510bb776d',
     authorityBase: 'https://fixitb2ctest.b2clogin.com/tfp/b590826f-dbb4-4f2c-9f72-cfc9ea116b5d',
-    redirectUrl: 'msauth.org.reactjs.native.example.FixitReactNative://auth',
+    redirectUrl: 'msauth.fixit.dev.ios://auth',
     policies: {
       signIn: 'B2C_1A_SIGNIN',
       signUp: 'B2C_1A_SIGNUP',
